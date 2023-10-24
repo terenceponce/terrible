a2fdfae1ea02b70bdb833e25703e1ed58f9c90ff
Terence Ponce <terence@terrible.finance>
2023-10-24 11:53:51 +0100

    Merge Book Live templates to LiveView files (#13)
    I prefer the React-style of creating components where the template is
    part of the component file itself.

--- lib/terrible_web/live/book_live/show.ex
+++ lib/terrible_web/live/book_live/show.ex
@@ -4,6 +4,39 @@ defmodule TerribleWeb.BookLive.Show do
   alias Terrible.Budgeting
 
   @impl true
+  def render(assigns) do
+    ~H"""
+    <.header>
+      Book <%= @book.id %>
+      <:subtitle>This is a book record from your database.</:subtitle>
+      <:actions>
+        <.link patch={~p"/books/#{@book}/show/edit"} phx-click={JS.push_focus()}>
+          <.button>Edit book</.button>
+        </.link>
+      </:actions>
+    </.header>
+
+    <.list>
+      <:item title="Name"><%= @book.name %></:item>
+    </.list>
+
+    <.back navigate={~p"/books"}>Back to books</.back>
+
+    <.modal :if={@live_action == :edit} id="book-modal" show on_cancel={JS.patch(~p"/books/#{@book}")}>
+      <.live_component
+        module={TerribleWeb.BookLive.FormComponent}
+        id={@book.id}
+        title={@page_title}
+        action={@live_action}
+        book={@book}
+        patch={~p"/books/#{@book}"}
+        current_user_id={@current_user.id}
+      />
+    </.modal>
+    """
+  end
+
+  @impl true
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
